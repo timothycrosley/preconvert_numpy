4689b9698ea939b396b8147d0c62f2032120e9f7
Timothy Crosley <timothy.crosley@gmail.com>
2019-07-25 22:59:04 -0700

    Import converters

--- preconvert_numpy/__init__.py
+++ preconvert_numpy/__init__.py
@@ -1,4 +1,6 @@
 """Preconverts common numpy types to their serializable (jsonifiable, msgpackable, etc...) form.
 A plugin for the preconvert Python library.
 """
+import converters
+
 __version__ = "0.0.1"
