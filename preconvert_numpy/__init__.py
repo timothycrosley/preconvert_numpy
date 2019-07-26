bf0904a4b800050d103fbd080cf354c981e1e1a9
Timothy Crosley <timothy.crosley@gmail.com>
2019-07-25 22:38:06 -0700

    Add initial converters for numpy types

---
+++ preconvert_numpy/__init__.py
@@ -0,0 +1,4 @@
+"""Preconverts common numpy types to their serializable (jsonifiable, msgpackable, etc...) form.
+A plugin for the preconvert Python library.
+"""
+__version__ = "0.0.1"
---
+++ preconvert_numpy/converters.py
@@ -0,0 +1,26 @@
+import numpy
+
+
+@register.converter(numpy.ndarray)
+def numpy_listable(item):
+    return item.tolist()
+
+@register.converter(str, numpy.unicode_)
+def numpy_stringable(item):
+    return str(item)
+
+@register.converter(numpy.bytes_)
+def numpy_byte_decodeable(item):
+    return item.decode()
+
+@register.converter(numpy.bool_)
+def numpy_boolable(item):
+    return bool(item)
+
+@register.converter(numpy.integer)
+def numpy_integerable(item):
+    return int(item)
+
+@register.converter(float, numpy.floating)
+def numpy_floatable(item):
+    return float(item)
